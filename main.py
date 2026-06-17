fe9b10fe3b61582f90c045bc6f9a8503aa1120a8
Bianca Portes <bianca.portes.c@gmail.com>
2026-06-16 22:08:57 -0300

    Código refinado para versão final

--- main.py
+++ main.py
@@ -11,39 +11,10 @@ import calculafrequencia as calcFreq
 import os
 
 if __name__ == '__main__':
-
-    for i in range(1, 4):
-        pasta = "ano" + str(i)
-        if not os.path.exists(pasta):
-            os.mkdir(pasta)
-
-    #ETAPAS
-    #1) Gero os relatórios no Sigaa em HTML e salvo nas respectivas pastas referentes ao ano do técnico: ano1, ano2 e ano3
-    #   Dá pra usar o Selenium/Python pra fazer isso, mas não fiz.
-
-    print('''Olá! Antes de tudo, lembre-se de salvar o último arquivo de conselho de classe nas respectivas pastas!
-    - 1º ano: pasta ano1
-    - 2º ano: pasta ano2
-    - 3º ano: pasta ano3
-
-    ATENÇÃO! Os arquivos de conselho de classe gerados pelo Sigaa devem ser salvos como HTML.
-    ''')
-    input("SE ESTIVER TUDO PRONTO, APERTE ENTER!")
-
-    trimestre = int(input("\n\nEm qual trimestre estamos? (1, 2 ou 3)"))
-
-    resposta = input("Trata-se do Técnico Integrado em Informática? (sim ou nao)").upper()
-    if resposta in ('SIM', 'S'):
-        #1o = 760+160+400, 2o = 760+200+320 e 3o = 720+240+240
-        nAulas1o, nAulas2o, nAulas3o = 1320, 1280, 1200
-    else:
-        nAulas1o = int(input("\t\t 1o ano - qual o número de aulas? "))
-        nAulas2o = int(input("\t\t 2o ano - qual o número de aulas? "))
-        nAulas3o = int(input("\t\t 3o ano - qual o número de aulas? "))
-    limiarReprovFreq = calcFreq.calculaLimiarPerigoReprovacaoFreq(nAulas1o, nAulas2o, nAulas3o, trimestre)
-
-    print(limiarReprovFreq)
-
+    #1) Pega as informações gerais do curso
+    man = ManipuladorArquivos("ano1/")
+    man.abre_arquivo_html()
+    matriz = man.geraAmbiente()
 
     for ano in range(1, 4):
         #2) Altero a variável 'path' para dizer qual a pasta eu quero que a análise seja feita
@@ -53,16 +24,17 @@ if __name__ == '__main__':
         man.abre_arquivo_html()
         df = man.abreArquivoCSV()
 
-        listaFreqTodosAlunos = calcFreq.somaFaltas(df, limiarReprovFreq[ano-1], trimestre)
+        listaFreqTodosAlunos = calcFreq.somaFaltas(df, matriz['limiarRepr'][str(ano)], matriz['trimestre'])
 
         #Gera arquivo na pasta com os nomes das disciplinas daquele ano
         man.geraArquivoComNomeDisciplinas(df,ordenar = False)
 
         #5) Lê os nomes das disciplinas daquele ano
         disciplinas = man.todasDisciplinas()
+        
 
-        arqSaida = df.loc[1:,'NOME'] #nomes alunos
-
+        arqSaida = df.loc[1:,'NOMES'] #nomes alunos
+        
         #Inicio a junção dos arquivos CSVs para cada disciplina
         for disc in disciplinas:
             disc = disc.strip() #Retirei o '\n' da 1ª disciplina
@@ -76,3 +48,4 @@ if __name__ == '__main__':
 
 
         print("Arquivos gerados! Confira nas pastas.")
+        
--- manipulaarquivos.py
+++ manipulaarquivos.py
@@ -7,32 +7,92 @@
 
 import pandas as pd
 import os
+from bs4 import BeautifulSoup
+import calculafrequencia as calcFreq
+import re
+
 
 class ManipuladorArquivos:
     def __init__(self, path: str):
         self.path = path
+        self.metadata = {}
+        self.info_curso = {}
 
     def abre_arquivo_html(self):
         arquivos = [f for f in os.listdir(self.path) if f.endswith(".html")]
 
-        if (len(arquivos) > 1) or (len(arquivos) == 0) :
-            try:
-                raise KeyboardInterrupt
-            finally:
-                print("#"*50)
-                print('''
-                ATENÇÃO!!!
-
-                Deixe apenas UM arquivo .HTML referente ao conselho na pasta ''', self.path, "\n\n")
-
-                print("#"*50)
+        if len(arquivos) != 1:
+            raise ValueError(
+                f"Esperado exatamente 1 arquivo .HTML em '{self.path}', "
+                f"encontrado {len(arquivos)}."
+            )
 
         caminho = os.path.join(self.path , arquivos[0])
-        df = pd.read_html(caminho)[0]
-        df = df.rename(columns={"Unnamed: 1_level_0": 'NOME'})
-        self.geraArquivoCSV(df, caminho[:-4] + "csv" )
 
+        # Extrai metadados do cabeçalho
+        with open(caminho, encoding='windows-1252') as f:
+            soup = BeautifulSoup(f, 'html.parser')
+
+        itens = soup.find_all('div', style=lambda s: s and 'font-weight: bold' in s)
+        for item in itens:
+            texto = item.text.strip()
+            if ':' in texto:
+                chave, valor = texto.split(':', 1)
+                self.metadata[chave.strip()] = valor.strip()
+
+
+        df = pd.read_html(caminho, encoding='windows-1252')[0]
+        df = df.rename(columns={"Unnamed: 1_level_0": 'NOMES'})
+        df = df.rename(columns=lambda col: re.sub(r'^Unnamed:.*', '', col))
         
+        self.geraArquivoCSV(df, caminho[:-4] + "csv" )
+        #return self.metadata
+        
+
+    def geraAmbiente(self):
+        for i in range(1, 4):
+            pasta = "ano" + str(i)
+            if not os.path.exists(pasta):
+                os.mkdir(pasta)
+
+        #ETAPAS
+        #1) Gero os relatórios no Sigaa em HTML e salvo nas respectivas pastas referentes ao ano do técnico: ano1, ano2 e ano3
+        #   Dá pra usar o Selenium/Python pra fazer isso, mas não fiz.
+
+        print('''Olá! Antes de tudo, lembre-se de salvar o último arquivo de conselho de classe nas respectivas pastas!
+        - 1º ano: pasta ano1
+        - 2º ano: pasta ano2
+        - 3º ano: pasta ano3
+
+        ATENÇÃO! Os arquivos de conselho de classe gerados pelo Sigaa devem ser salvos como HTML.
+        ''')
+        input("SE ESTIVER TUDO PRONTO, APERTE ENTER!")
+
+        ano = self.metadata['Módulo'].split()[1]
+        trimestre = int(self.metadata['Bimestre/Trimestre'])
+        curso = self.metadata['Estrutura Curricular']
+
+        if curso == 'TÉCNICO EM INFORMÁTICA - TIINF05003 (INT) [0320251 - 2025]':
+            nAulas1o, nAulas2o, nAulas3o = 1240,1240,1160
+        elif curso == 'TÉCNICO EM INFORMÁTICA - TIINF05003 (INT) [0320231 - 2023]':
+            #1o = 760+160+400, 2o = 760+200+320 e 3o = 720+240+240
+            nAulas1o, nAulas2o, nAulas3o = 1320, 1280, 1200
+        else:
+            print(f"O curso {curso} não tem o número de aulas por ano cadastrados no sistema\nInforme abaixo:\n")
+            nAulas1o = int(input("\t\t 1o ano - qual o número de aulas? "))
+            nAulas2o = int(input("\t\t 2o ano - qual o número de aulas? "))
+            nAulas3o = int(input("\t\t 3o ano - qual o número de aulas? "))
+        limiarReprovFreq = calcFreq.calculaLimiarPerigoReprovacaoFreq(nAulas1o, nAulas2o, nAulas3o, trimestre)
+
+
+        self.info_curso = {
+            'curso': curso,
+            'trimestre': trimestre,
+            'matriz': {'1':nAulas1o, '2': nAulas2o, '3': nAulas3o},
+            'limiarRepr': {'1':limiarReprovFreq[0], '2': limiarReprovFreq[1], '3': limiarReprovFreq[2]}
+        }
+
+        return self.info_curso
 
     def geraArquivoCSV(self, df, caminho):
         #apaga arquivos .csv que estejam na pasta
@@ -46,9 +106,9 @@ class ManipuladorArquivos:
     def abreArquivoCSV(self):
         arquivos = [_ for _ in os.listdir(self.path) if _.endswith(".csv")]
 
-        if (len(arquivos) != 1) :
+        if len(arquivos) != 1:
             raise ValueError(
-                f"Esperado exatamente 1 arquivo .HTML em '{self.path}', "
+                f"Esperado exatamente 1 arquivo .CSV em '{self.path}', "
                 f"encontrado {len(arquivos)}."
             )
 
@@ -56,9 +116,9 @@ class ManipuladorArquivos:
         return pd.read_csv(caminho)
 
 
-    def geraArquivoComNomeDisciplinas(self, ultimoTrimestre, ordenar: bool = True):
+    def geraArquivoComNomeDisciplinas(self, df, ordenar: bool = True):
         COLUNAS_POR_DISCIPLINA = 4  # N1, R, F, S
-        colunas = ultimoTrimestre.iloc[0].index[4:]  # pula as 4 primeiras (matrícula, nome, etc.)
+        colunas = df.iloc[0].index[4:]  # pula as 4 primeiras (matrícula, nome, etc.)
         
         nomes_disciplinas = []
         for i, col in enumerate(colunas):
